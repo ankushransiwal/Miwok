734b342c41b6c209f9e25fec8f9f559424f3276a
Ankush <ankushransiwal.13@gmail.com>
2018-07-06 16:38:30 +0530

    Use adapter and list view for number category

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,28 @@
+package com.example.android.miwok;
+
+public class Word {
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
